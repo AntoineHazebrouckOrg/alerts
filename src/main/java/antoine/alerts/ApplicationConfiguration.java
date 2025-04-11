57353ec7ecb264822c3932f6aae3fa3b1d33a48b
Antoine Hazebrouck <antoine.hazebrouck.etu@univ-lille.fr>
2025-04-11 17:52:01 +0200

    config setup

--- src/main/java/antoine/alerts/ApplicationConfiguration.java
+++ src/main/java/antoine/alerts/ApplicationConfiguration.java
@@ -1,13 +1,15 @@
 package antoine.alerts;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
-@Setter(value = AccessLevel.PRIVATE)
+@Setter(value = AccessLevel.PACKAGE) // only spring should use setters
 @Data
+@Configuration
 @ConfigurationProperties(prefix = "application")
 public class ApplicationConfiguration {
 	private String receiver;
