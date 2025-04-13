a5e869ce08708fd37f335ab23b420e22b2c4ca38
Antoine Hazebrouck <antoine.hazebrouck.etu@univ-lille.fr>
2025-04-13 14:40:02 +0200

    can add recipe

--- src/main/java/antoine/alerts/controllers/RecipesController.java
+++ src/main/java/antoine/alerts/controllers/RecipesController.java
@@ -5,22 +5,35 @@ import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import antoine.alerts.data.recipes.Recipe;
 import antoine.alerts.repositories.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/")
+@RequestMapping("/recipes")
 @RequiredArgsConstructor
 @Controller
-public class IndexController {
+public class RecipesController {
+	private static final String newRecipeAttribute = "recipe";
+
 	private final RecipeRepository recipeRepository;
 
 	@GetMapping
 	public String get(Model model) throws IOException {
 		model.addAttribute("recipes", recipeRepository.findAll());
+		model.addAttribute(newRecipeAttribute, new Recipe());
 
-		return "index";
+		return "recipes";
+	}
+
+	@PostMapping
+	public String post(@ModelAttribute(name = newRecipeAttribute) Recipe recipe, Model model) throws IOException {
+		recipeRepository.save(recipe);
+
+		return "redirect:recipes";
 	}
 
 }
